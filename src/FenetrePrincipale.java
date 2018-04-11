30a18fb6256b33642e6c8868a4dec583f12a5fa0
Mathieu <mathieu_tgravel@hotmail.com>
2018-04-11 16:22:00 -0400

    Setup.

---
+++ src/FenetrePrincipale.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class FenetrePrincipale {
+    private JFrame fenetreDemarage;
+
+    public FenetrePrincipale() {
+        initialisationDeFenetre();
+    }
+
+    private void initialisationDeFenetre() {
+
+    }
+}
