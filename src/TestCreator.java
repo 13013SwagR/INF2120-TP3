bcc195a05181c79f8ac605d7589f0ce09ef7fd44
Vincent Dansereau <dansereauvincent@gmail.com>
2018-04-18 17:14:35 -0400

    Classe TestCreator : Initialisation du JFrame

--- src/TestCreator.java
+++ src/TestCreator.java
@@ -1,28 +1,113 @@
 import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class TestCreator {
-    public JFrame testCreatorWindow;
-    public JButton previousButton;
-    public JButton nextButton;
-    public JButton addButton;
-    public JButton removeButton;
-    public JButton saveButton;
+    private final static JLabel TEST_NAME_LABEL = new JLabel("Nom du test");
+    private final static JLabel QUESTION = new JLabel("QUESTION");
+    private final static JLabel STATEMENT = new JLabel("Énoncé");
+    private final static JLabel ANSWER_OPTIONS = new JLabel("Choix de réponses "
+            + "(cochez la bonne réponse");
+    private final static JLabel OPTION_1 = new JLabel("1)");
+    private final static JLabel OPTION_2 = new JLabel("2)");
+    private final static JLabel OPTION_3 = new JLabel("3)");
+    private final static JLabel OPTION_4 = new JLabel("4)");
+    
+    private final static int JFRAME_WIDTH = 550;
+    private final static int JFRAME_HEIGHT = 540;
+    
+    private final static String WINDOW_NAME = "Créer un test";
+    private final static String PREVIOUS_BUTTON_NAME = "<";
+    private final static String NEXT_BUTTON_NAME = ">";
+    private final static String ADD_BUTTON_NAME = "+";
+    private final static String REMOVE_BUTTON_NAME = "-";
+    private final static String SAVE_BUTTON_NAME = "Sauvegarder le test";
+    
+    private static int questionsIndex = 0;
+    
+    private JFrame testCreatorWindow;
+    
+    private JButton previousButton;
+    private JButton nextButton;
+    private JButton addButton;
+    private JButton removeButton;
+    private JButton saveButton;
+    
+    private JLabel questionNumber;
+    
+    private JTextField questionStatement;
+    private JTextField answer1;
+    private JTextField answer2;
+    private JTextField answer3;
+    private JTextField answer4;
+    
+    private JCheckBox boxAnswer1;
+    private JCheckBox boxAnswer2;
+    private JCheckBox boxAnswer3;
+    private JCheckBox boxAnswer4;
     
     // TODO: Créer une fenêtre.
     
     public TestCreator () {
-        this.testCreatorWindow  = new JFrame("Fenêtre de création de testes");
-        this.testCreatorWindow.setSize(380, 360);
+        initTestCreatorWindow();
+        this.previousButton = new JButton(PREVIOUS_BUTTON_NAME);
+        this.nextButton = new JButton(NEXT_BUTTON_NAME);
+        this.addButton = new JButton(ADD_BUTTON_NAME);
+        this.removeButton = new JButton(REMOVE_BUTTON_NAME);
+        this.saveButton = new JButton(SAVE_BUTTON_NAME);
+        
+        questionsIndex++;
+        
+        this.questionNumber = new JLabel(Integer.toString(questionsIndex));
     
-        this.previousButton = new JButton("<");
-        this.nextButton = new JButton(">");
-        this.addButton = new JButton("+");
-        this.removeButton = new JButton("-");
-        this.saveButton = new JButton("Sauvegarder le test");
+        this.questionStatement = new JTextField();
+        this.answer1 = new JTextField();
+        this.answer2 = new JTextField();
+        this.answer3 = new JTextField();
+        this.answer4 = new JTextField();
+    
+        this.boxAnswer1 = new JCheckBox();
+        this.boxAnswer2 = new JCheckBox();
+        this.boxAnswer3 = new JCheckBox();
+        this.boxAnswer4 = new JCheckBox();
     }
     
+    private void initTestCreatorWindow() {
+        this.testCreatorWindow  = new JFrame(WINDOW_NAME);
+        this.testCreatorWindow.setSize(JFRAME_WIDTH, JFRAME_HEIGHT);
+        this.positionTestCreatorWindow();
+        this.testCreatorWindow.setResizable(false);
+        this.testCreatorWindow.setVisible(true);
+        // TODO: Effacer avant la remise
+        this.testCreatorWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
     
-    
+    private void positionTestCreatorWindow() {
+        double screenWidth = java.awt.Toolkit.getDefaultToolkit()
+                .getScreenSize().getWidth();
+        double screenHeight = java.awt.Toolkit.getDefaultToolkit()
+                .getScreenSize().getHeight();
+        int x = (int) ( (screenWidth / 2) - (JFRAME_WIDTH / 2) );
+        int y = (int) ( (screenHeight / 2)  - (JFRAME_WIDTH / 2) );
+        this.testCreatorWindow.setLocation(x,y);
+    }
+
+    public static void main (String [] args)  {
+
+        new TestCreator();
+
+        // OU Exécution d'un thread, MAJ du GUI
+      /*
+      javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Exemple1JFrame();
+            }
+      });
+      */
+    }
     /** TODO: Bouton [ < ] :
     Ce bouton sert à revenir à la question qui précède la question courante. Il doit être désactivé
     lorsque la question courante est la première question de ce test.
