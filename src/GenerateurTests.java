ff51004a2292d0361e165e2300bb51937e65b384
Mathieu <mathieu_tgravel@hotmail.com>
2018-04-19 17:56:57 -0400

    fenetre principale finie (apparence)

--- src/GenerateurTests.java
+++ src/GenerateurTests.java
@@ -1,21 +1,24 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class GenerateurTests {
     /************************************
      * VARIABLES D'INSTANCES
      ************************************/
     private JFrame fenetreDemarage;
-    private JButton boutonCreer;
-    private JButton boutonPasser;
-    private JButton boutonSupprimer;
+    private JButton buttonCreer;
+    private JButton buttonPasser;
+    private JButton buttonSupprimer;
     private JComboBox<String> listeDeTests;
+    private JPanel blackLine;
 
 
     /************************************
      * CONSTANTES DE CLASSE
      ************************************/
-    public final static int HAUT_BOUTON = 20;
-    public final static int LARGEUR_BOUTON = 200;
+    public final static int HAUT_BOUTON = 30;
+    public final static int LARGEUR_BOUTON = 250;
 
 
 
@@ -39,12 +42,24 @@ public class GenerateurTests {
         /*****************************
          * INITIALISATION DES BOUTONS*
          *****************************/
-        boutonCreer = new JButton("Créer un nouveau test");
-        boutonCreer.setBounds(
-                fenetreDemarage.getWidth()/2 - LARGEUR_BOUTON/2
-                ,(fenetreDemarage.getHeight()/2)-120
-                ,LARGEUR_BOUTON,HAUT_BOUTON );
-        fenetreDemarage.getContentPane().add(boutonCreer);
+        buttonCreer = createJButton("Créer un nouveau test", 150);
+        fenetreDemarage.getContentPane().add(buttonCreer);
+
+        buttonPasser = createJButton("Passer le test sélectionné", -30);
+        fenetreDemarage.getContentPane().add(buttonPasser);
+
+        buttonSupprimer = createJButton("Supprimer le test sélectionné", -80);
+        fenetreDemarage.getContentPane().add(buttonSupprimer);
+
+        /************************
+         * AFFICHAGE D'UN JPanel*
+         ************************/
+        blackLine = new JPanel();
+        blackLine.setSize(fenetreDemarage.getWidth()-40,1);
+        blackLine.setLocation((fenetreDemarage.getWidth()-blackLine.getWidth())/2, (fenetreDemarage.getHeight()/2)-85);
+        blackLine.setBackground(Color.black);
+
+        fenetreDemarage.getContentPane().add(blackLine);
 
 
         /***********************************
@@ -56,9 +71,9 @@ public class GenerateurTests {
 
         listeDeTests.setBounds(
                 (fenetreDemarage.getWidth() - LARGEUR_BOUTON)/2,
-                (fenetreDemarage.getHeight()/2)+20,
+                (fenetreDemarage.getHeight()/2)-40,
                 LARGEUR_BOUTON,
-                HAUT_BOUTON);
+                HAUT_BOUTON-10);
 
         fenetreDemarage.getContentPane().add(listeDeTests);
         /**************************
@@ -69,8 +84,16 @@ public class GenerateurTests {
 
 
     }
+    public JButton createJButton(String buttonName, int height){
+        JButton newJButton = new JButton(buttonName);
+        newJButton.setBounds(
+                fenetreDemarage.getWidth()/2 - LARGEUR_BOUTON/2
+                ,(fenetreDemarage.getHeight()/2)-height
+                ,LARGEUR_BOUTON,HAUT_BOUTON );
+        return newJButton;
+    }
     public static void main (String [] args) {
 
-        new FenetrePrincipale();
+        new GenerateurTests();
     }
 }
