146ca8e7b046212932d572014a516652609869fc
Vincent Dansereau <dansereauvincent@gmail.com>
2018-04-20 13:44:02 -0400

    Classe GenerateurTests : Refactoring

--- src/GenerateurTests.java
+++ src/GenerateurTests.java
@@ -1,7 +1,6 @@
 //import com.sun.xml.internal.bind.v2.TODO;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,106 +10,59 @@ public class GenerateurTests {
     /************************************
      * VARIABLES D'INSTANCES
      ************************************/
-    private JFrame fenetreDemarage;
-    private JButton buttonCreer;
-    private JButton buttonPasser;
-    private JButton buttonSupprimer;
-    private JComboBox<String> listeDeTests;
-    private JPanel blackLine;
-
-    private ActionListener creerListener;
-
-    TestCreator testCreation;
-
-    private ArrayList<TestCreator> createdTests = new ArrayList<>();
-
-
+    private static JFrame startUpWindow;
+    private static ActionListener creerListener;
+    
+    static TestCreator testCreation;
+    
+    private static ArrayList<TestCreator> createdTests = new ArrayList<>();
+    
+    
     /************************************
      * CONSTANTES DE CLASSE
      ************************************/
     public final static int HAUT_BOUTON = 30;
     public final static int LARGEUR_BOUTON = 250;
-
-
-
-    public GenerateurTests() {
-        initialisationDeFenetre();
-    }
-
-    private void initialisationDeFenetre() {
-
-        /************************************
-         * INIT FENETRE PRINCIPALE (JFRAME)
-         ************************************/
-
+    
+    
+    public static void initStartUpWindow() {
+        
+        JButton createNewTestButton;
+        JButton buttonPasser;
+        JButton buttonSupprimer;
+        JComboBox<String> testsList;
+        
+        // INIT FENETRE PRINCIPALE (JFRAME)
+        startUpWindow = new JFrame("Générateur de tests");
+        startUpWindow.setBounds(getWindowPositionX(), getWindowPositionY(), 380, 360);
+        startUpWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        startUpWindow.setResizable(false);
+        startUpWindow.setLayout(null);  //pas de layout manager
+        
+        // INITIALISATION DES BOUTONS
+        createNewTestButton = createStandardJButton("Créer un nouveau test", 150);
+        startUpWindow.getContentPane().add(createNewTestButton);
+        
+        buttonPasser = createStandardJButton("Passer le test sélectionné", -30);
+        startUpWindow.getContentPane().add(buttonPasser);
+        
+        buttonSupprimer = createStandardJButton("Supprimer le test sélectionné", -80);
+        startUpWindow.getContentPane().add(buttonSupprimer);
+        
         //creation et initialisation des proprietes de la fenetre principale
-
-        double screenWidth = java.awt.Toolkit.getDefaultToolkit()
-                .getScreenSize().getWidth();
-        double screenHeight = java.awt.Toolkit.getDefaultToolkit()
-                .getScreenSize().getHeight();
-        int x = (int) ((screenWidth / 2) - (HAUT_BOUTON / 2));
-        int y = (int) ((screenHeight / 2) - (LARGEUR_BOUTON / 2));
-
-        fenetreDemarage = new JFrame("Générateur de tests");
-        fenetreDemarage.setBounds(x, y, 380, 360);
-        fenetreDemarage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetreDemarage.setResizable(false);
-        fenetreDemarage.setLayout(null);  //pas de layout manager
-
-        /*****************************
-         * INITIALISATION DES BOUTONS*
-         *****************************/
-        buttonCreer = createJButton("Créer un nouveau test", 150);
-        fenetreDemarage.getContentPane().add(buttonCreer);
-
-        buttonPasser = createJButton("Passer le test sélectionné", -30);
-        fenetreDemarage.getContentPane().add(buttonPasser);
-
-        buttonSupprimer = createJButton("Supprimer le test sélectionné", -80);
-        fenetreDemarage.getContentPane().add(buttonSupprimer);
-
-        /************************
-         * AFFICHAGE D'UN JPanel*
-         ************************/
-        blackLine = new JPanel();
-        blackLine.setSize(fenetreDemarage.getWidth()-40,1);
-        blackLine.setLocation((fenetreDemarage.getWidth()-blackLine.getWidth())/2, (fenetreDemarage.getHeight()/2)-85);
-        blackLine.setBackground(Color.black);
-
-        fenetreDemarage.getContentPane().add(blackLine);
-
-
-        /***********************************
-         * AFFICHAGE D'UNE LISTE DEROULANTE*
-         ***********************************/
-        listeDeTests = new JComboBox<String>();
-        for(int i = 0; i<createdTests.size(); i++){
-            //listeDeTests.add();
-            //createdTests.get(i).testNameAnswer;
-            // TODO create testNameAnswer in TestCreator
-
-        }
-        listeDeTests.addItem("blabla");
-        listeDeTests.setEnabled(true);
-
-        listeDeTests.setBounds(
-                (fenetreDemarage.getWidth() - LARGEUR_BOUTON)/2,
-                (fenetreDemarage.getHeight()/2)-40,
-                LARGEUR_BOUTON,
-                HAUT_BOUTON-10);
-
-        fenetreDemarage.getContentPane().add(listeDeTests);
-        /**************************
-         * AFFICHAGE DE LA FENETRE*
-         **************************/
-        fenetreDemarage.setVisible(true);
-
+        
+        startUpWindow.getContentPane().add(createStartUpWindowBlackLine());
+        
+        testsList = initTestsListComboBox();
+        startUpWindow.getContentPane().add(testsList);
+        
+        startUpWindow.setVisible(true);
+        
         /*******************
          * ACTIONS HANDLING*
          *******************/
         //Boutton creer
-
+        
         creerListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -118,21 +70,78 @@ public class GenerateurTests {
                 createdTests.add(testCreation);
             }
         };
-        buttonCreer.addActionListener(creerListener);
-
-
-
+        createNewTestButton.addActionListener(creerListener);
+        
+        
     }
-    public JButton createJButton(String buttonName, int height){
+    
+    private static JComboBox<String> initTestsListComboBox() {
+        JComboBox<String> testsList;
+        testsList = getTestList();
+        testsList.addItem("blabla");
+        testsList.setEnabled(true);
+        testsList.setBounds(
+                (startUpWindow.getWidth() - LARGEUR_BOUTON) / 2,
+                (startUpWindow.getHeight() / 2) - 40,
+                LARGEUR_BOUTON,
+                HAUT_BOUTON - 10);
+        return testsList;
+    }
+    
+    private static JComboBox<String> getTestList() {
+        JComboBox<String> listeDeTests;
+        listeDeTests = new JComboBox<String>();
+        for (int i = 0; i < createdTests.size(); i++) {
+            //listeDeTests.add();
+            //createdTests.get(i).testNameAnswer;
+            // TODO create testNameAnswer in TestCreator
+        }
+        return listeDeTests;
+    }
+    
+    private static JPanel createStartUpWindowBlackLine() {
+        JPanel blackLine;
+        blackLine = new JPanel();
+        blackLine.setSize(startUpWindow.getWidth() - 40, 1);
+        blackLine.setLocation((startUpWindow.getWidth() - blackLine.getWidth())
+                / 2, (
+                startUpWindow.getHeight() / 2) - 85);
+        blackLine.setBackground(Color.black);
+        return blackLine;
+    }
+    
+    private static int getWindowPositionX() {
+        double screenWidth = getScreenWidth();
+        return (int) ((screenWidth / 2) - (HAUT_BOUTON / 2));
+    }
+    
+    private static int getWindowPositionY() {
+        double screenHeight = getScreenHeight();
+        return (int) ((screenHeight / 2) - (LARGEUR_BOUTON / 2));
+    }
+    
+    
+    private static double getScreenHeight() {
+        return Toolkit.getDefaultToolkit()
+                .getScreenSize().getHeight();
+    }
+    
+    private static double getScreenWidth() {
+        return Toolkit.getDefaultToolkit()
+                .getScreenSize().getWidth();
+    }
+    
+    public static JButton createStandardJButton(String buttonName, int height) {
         JButton newJButton = new JButton(buttonName);
         newJButton.setBounds(
-                fenetreDemarage.getWidth()/2 - LARGEUR_BOUTON/2
-                ,(fenetreDemarage.getHeight()/2)-height
-                ,LARGEUR_BOUTON,HAUT_BOUTON );
+                startUpWindow.getWidth() / 2 - LARGEUR_BOUTON / 2
+                , (startUpWindow.getHeight() / 2) - height
+                , LARGEUR_BOUTON, HAUT_BOUTON);
         return newJButton;
     }
-    public static void main (String [] args) {
-
-        new GenerateurTests();
+    
+    public static void main(String[] args) {
+        
+        initStartUpWindow();
     }
 }
